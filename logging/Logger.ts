476f88b5ece23cb388221098e9cc24f847c3b744
JRReduta <jrreduta@yahoo.com>
2026-03-26 03:59:21 -0700

    (info) Removed todo

--- logging/Logger.ts
+++ logging/Logger.ts
@@ -1,14 +1,3 @@
-/**
- *
- * TODO:
- * (x) 1. migrate Logger to SillyStoreDTOs
- * (x) 2. Change SillyStoreDTOs title to SillyStoreCommon
- *  (x) 2.5. Explicitly set NODE_ENV in npm run dev to "development"
- * (x) 3. SillyStoreCommon - make config file that parses process.env
- * 4. SillyStoreCommon - make Logger take level from config, not process.env directly
- *
- */
-
 import { ILogObj, Logger } from "tslog";
 import configs from "../configs/Configs";
 
