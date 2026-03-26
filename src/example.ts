e01c59d9acbe76b1fabacfaaf4974f5afb7b810d
JRReduta <jrreduta@yahoo.com>
2026-03-26 03:10:36 -0700

    (change) example.ts and .env.example

--- src/example.ts
+++ src/example.ts
@@ -1,8 +1,28 @@
 import logger from "../logging/Logger";
+import dotenv from "dotenv";
 
-function main() {
+function checkLogger(): void {
+    logger.debug("dir is", __dirname);
     logger.info("This works!");
-    logger.debug("env vars from example:\n", process.env);
 }
 
+function checkDotEnv(): void {
+    const { DB_CONNECTION_STRING, JWT_SECRET, LOG_LEVEL, PORT } = process.env;
+    logger.debug("env vars from example:\n", [
+        DB_CONNECTION_STRING,
+        JWT_SECRET,
+        LOG_LEVEL,
+        PORT,
+    ]);
+}
+
+function main() {
+    checkLogger();
+    checkDotEnv();
+}
+
+dotenv.config({
+    debug: true,
+    path: __dirname + "/./../.env." + process.env.NODE_ENV,
+});
 main();
